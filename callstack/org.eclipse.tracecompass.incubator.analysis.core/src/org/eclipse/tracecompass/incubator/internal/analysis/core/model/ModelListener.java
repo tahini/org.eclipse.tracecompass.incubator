e7e4dcb6091ef5c118e32a5e593bd657741b8dbe
Geneviève Bastien <gbastien+lttng@versatic.net>
2017-05-03 22:17:08 -0400

    analysis: wrap the TID analysis in some providers

--- callstack/org.eclipse.tracecompass.incubator.analysis.core/src/org/eclipse/tracecompass/incubator/internal/analysis/core/model/ModelListener.java
+++ callstack/org.eclipse.tracecompass.incubator.analysis.core/src/org/eclipse/tracecompass/incubator/internal/analysis/core/model/ModelListener.java
@@ -9,13 +9,21 @@
 
 package org.eclipse.tracecompass.incubator.internal.analysis.core.model;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.WeakHashMap;
+
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.tracecompass.analysis.os.linux.core.tid.TidAnalysisModule;
 import org.eclipse.tracecompass.incubator.analysis.core.concepts.ICpuTimeProvider;
 import org.eclipse.tracecompass.incubator.analysis.core.concepts.IThreadOnCpuProvider;
 import org.eclipse.tracecompass.incubator.analysis.core.model.IHostModel;
 import org.eclipse.tracecompass.incubator.analysis.core.model.ModelManager;
 import org.eclipse.tracecompass.tmf.core.analysis.IAnalysisModule;
 import org.eclipse.tracecompass.tmf.core.analysis.ITmfNewAnalysisModuleListener;
+import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 
 /**
  * Listener for the {@link CompositeHostModel} that waits for new
@@ -24,6 +32,8 @@ import org.eclipse.tracecompass.tmf.core.analysis.ITmfNewAnalysisModuleListener;
  * @author Geneviève Bastien
  */
 public class ModelListener implements ITmfNewAnalysisModuleListener {
+
+    private Map<TidAnalysisModule, TidAnalysisWrapper> fTidModules = new WeakHashMap<>();
 
     @Override
     public void moduleCreated(@Nullable IAnalysisModule module) {
@@ -46,6 +56,39 @@ public class ModelListener implements ITmfNewAnalysisModuleListener {
                 }
             }
         }
+
+        if (module instanceof TidAnalysisModule) {
+            TidAnalysisModule provider = (TidAnalysisModule) module;
+            ITmfTrace trace = provider.getTrace();
+            if (trace != null) {
+                IHostModel model = ModelManager.getModelFor(trace.getHostId());
+                TidAnalysisWrapper tidAnalysisWrapper = new TidAnalysisWrapper(provider, trace.getHostId());
+                fTidModules.put(provider, tidAnalysisWrapper);
+                ((CompositeHostModel) model).setThreadOnCpuProvider(tidAnalysisWrapper);
+            }
+        }
+    }
+
+    private static class TidAnalysisWrapper implements IThreadOnCpuProvider {
+
+        private final TidAnalysisModule fModule;
+        private final Collection<String> fHostIds;
+
+        public TidAnalysisWrapper(TidAnalysisModule module, String hostId) {
+            fHostIds = Collections.singleton(hostId);
+            fModule = module;
+        }
+
+        @Override
+        public @Nullable Integer getThreadOnCpuAtTime(int cpu, long time) {
+            return fModule.getThreadOnCpuAtTime(cpu, time);
+        }
+
+        @Override
+        public @NonNull Collection<@NonNull String> getHostIds() {
+            return fHostIds;
+        }
+
     }
 
 }
