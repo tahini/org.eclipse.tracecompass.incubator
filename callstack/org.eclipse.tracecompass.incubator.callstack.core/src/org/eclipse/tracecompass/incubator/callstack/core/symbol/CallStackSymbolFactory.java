08d1ce81272859d7ddb8c8e74739e2625fff3ad6
Geneviève Bastien <gbastien+lttng@versatic.net>
2017-08-02 16:28:54 -0400

    callstack: Improve symbol resolution
    Add support of multiple symbol providers, updating to Trace Compass's
    most recent API.
    
    Also add a ICallstackSymbol interface for different types of symbol,
    that can be resolved to something using symbol providers, or simple
    strings.
    
    This patch depends on Trace Comapss's patch:
    https://git.eclipse.org/r/#/c/102987/
    
    Change-Id: Ie9352e0ecda87835d3e4c3b86221b344926b189d
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>

---
+++ callstack/org.eclipse.tracecompass.incubator.callstack.core/src/org/eclipse/tracecompass/incubator/callstack/core/symbol/CallStackSymbolFactory.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2017 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.incubator.callstack.core.symbol;
+
+import org.eclipse.tracecompass.incubator.callstack.core.base.ICallStackElement;
+import org.eclipse.tracecompass.incubator.internal.callstack.core.symbol.ResolvableSymbol;
+import org.eclipse.tracecompass.incubator.internal.callstack.core.symbol.StringSymbol;
+
+/**
+ * A factory that creates the appropriate symbol for a given element
+ *
+ * @author Geneviève Bastien
+ */
+public final class CallStackSymbolFactory {
+
+    private CallStackSymbolFactory() {
+        // Utility class, not to be instantiated
+    }
+
+    /**
+     * @param symbol
+     * @param element
+     * @param timestamp
+     * @return
+     */
+    public static ICallStackSymbol createSymbol(Object symbol, ICallStackElement element, long timestamp) {
+        if (symbol instanceof Long || symbol instanceof Integer) {
+            long longAddress = ((Long) symbol).longValue();
+            return new ResolvableSymbol(longAddress, element.getSymbolKeyAt(timestamp), timestamp);
+        }
+        if (symbol instanceof String) {
+            try {
+                long longAddress = Long.parseUnsignedLong((String) symbol, 16);
+                return new ResolvableSymbol(longAddress, element.getSymbolKeyAt(timestamp), timestamp);
+            } catch (NumberFormatException e) {
+                // Not a long number, use a string symbol
+            }
+        }
+        return new StringSymbol(symbol);
+    }
+}
