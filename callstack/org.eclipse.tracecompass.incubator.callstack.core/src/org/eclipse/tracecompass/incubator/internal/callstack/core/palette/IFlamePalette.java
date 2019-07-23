8288eef1a2fb6457ed00de45408c668c71d0697c
Geneviève Bastien <gbastien+lttng@versatic.net>
2019-07-23 11:39:02 -0400

    callstack: Have flame graph implement IOutputProvider
    Change-Id: I59abbfe6da4a1f0897ab224c61482c0437358055
    Signed-off-by: Geneviève Bastien <gbastien+lttng@versatic.net>

---
+++ callstack/org.eclipse.tracecompass.incubator.callstack.core/src/org/eclipse/tracecompass/incubator/internal/callstack/core/palette/IFlamePalette.java
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ * Copyright (c) 2019 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *******************************************************************************/
+
+package org.eclipse.tracecompass.incubator.internal.callstack.core.palette;
+
+import java.util.Map;
+
+import org.eclipse.tracecompass.tmf.core.model.OutputElementStyle;
+
+/**
+ * @author Geneviève Bastien
+ */
+public interface IFlamePalette {
+
+    /**
+     * @param callsite
+     * @return
+     */
+    OutputElementStyle getStyleFor(Object callsite);
+
+    /**
+     * @return
+     */
+    Map<String, OutputElementStyle> getStyles();
+
+}
